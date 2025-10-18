9d9a55cc7317904c920d7f02f04570b25d08ebf8
harsha <ashok66864@gmail.com>
2025-10-18 12:45:02 +0530

    React Sample

---
+++ FrontEnd/ReactSample.js
@@ -0,0 +1,7 @@
+import React from "https://esm.sh/react"
+import ReactDOM from "https://esm.sh/react-dom/client"
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+const childElt = <div className = "container"><h1>Hello Harsha</h1></div>
+
+root.render(childElt);
