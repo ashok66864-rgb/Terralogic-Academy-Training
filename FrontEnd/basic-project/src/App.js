1210308944ccd37aea5fb700a20192ebd2067c78
harsha <ashok66864@gmail.com>
2025-10-19 09:20:00 +0530

    commit

---
+++ FrontEnd/basic-project/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Input from './components/Input.js';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Harsha</h1>
+      <Input/>
+    </div>
+  );
+}  
+
+export default App;
---
+++ FrontEnd/basic-project/src/components/Input.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Demo from './Demo'
+const Input = () => {
+  return (
+    <div>
+        <Demo/>
+        <input type='text' placeholder='Enter your name' />
+    </div>
+  )
+}
+
+export default Input; 
